3c393e5ee367bc9435c572a3713cda3150f7bbbf
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-04 15:21:56 +0300

    new architecture

--- game.js
+++ game.js
@@ -1,10 +1,18 @@
 class Game {
-  constructor() {
+  constructor(scene, camera) {
+    this.initializeScene(scene, camera)
+
+    //bind callbacks
     document.addEventListener('keydown', this._keydown.bind(this))
     document.addEventListener('keyup', this._keyup.bind(this))
   }
   update() {
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+
     this.updateGrid()
+    this.checkCollision()
+    this.updateInfo()
   }
 
   _keydown(event) {}
@@ -12,4 +20,17 @@ class Game {
   _keyup() {}
 
   updateGrid() {}
+
+  checkCollision() {}
+
+  updateInfo() {}
+
+  gameOver() {}
+
+  initializeScene() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    this.cube = new THREE.Mesh(geometry, material)
+    scene.add(this.cube)
+  }
 }
