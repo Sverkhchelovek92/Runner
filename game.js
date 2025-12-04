06e1a32bd22f817d3e5573e6b0055d7b7b091962
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-04 12:45:25 +0300

    game class

--- game.js
+++ game.js
@@ -1,4 +1,15 @@
 class Game {
-  constructor() {}
-  update() {}
+  constructor() {
+    document.addEventListener('keydown', this._keydown.bind(this))
+    document.addEventListener('keyup', this._keyup.bind(this))
+  }
+  update() {
+    this.updateGrid()
+  }
+
+  _keydown(event) {}
+
+  _keyup() {}
+
+  updateGrid() {}
 }
