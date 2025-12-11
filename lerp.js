7efc4ab5a76c6093c440c8de09000f8bd665ca64
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-12-11 15:38:40 +0300

    lerp

--- lerp.js
+++ lerp.js
@@ -1,5 +1,40 @@
 class Lerp {
-  constructor() {}
+  constructor(from, to, delay) {
+    this.from = from
+    this.to = to
+    this.delay = delay
 
-  update(timeDelta) {}
+    this.time = 0
+    this.value = from
+
+    this.lerpSpeed = 1 / this.delay
+
+    return this
+  }
+
+  update(timeDelta) {
+    const t = this.time / this.delay
+    this.value = this.from * (1 - t) + this.to * t
+
+    this.time += timeDelta * this.lerpSpeed
+
+    if (this.onUpdateCallback) this.onUpdateCallback(this.value)
+
+    if (this.time >= this.delay) {
+      if (this.onFinishCallback) this.onFinishCallback()
+      delete this
+    }
+  }
+
+  onUpdate(callback) {
+    this.onUpdateCallback = callback
+
+    return this
+  }
+
+  onFinish(callback) {
+    this.onFinishCallback = callback
+
+    return this
+  }
 }
